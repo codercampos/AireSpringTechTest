def3deaa16a5d7c64ad2681b3eb10a0528216cd7
Carlos Campos <me@carloscampos.dev>
2023-10-04 09:07:58 -0600

    Clean up and added Add button in home

--- Models/EmployeeViewModel.cs
+++ Models/EmployeeViewModel.cs
@@ -1,6 +1,4 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace AireSpringTechTest.Models;
 public class EmployeeViewModel
